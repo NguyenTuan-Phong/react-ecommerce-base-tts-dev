84e3782c8bda6644bbb99654bf28b8849f86964b
NguyenTuan-Phong <nguyentuanphong2802@gmail.com>
2025-09-10 10:07:25 +0700

    a
    a

--- src/services/api_InfoUser.ts
+++
@@ -1,9 +0,0 @@
-import { get } from "../config/axios-config";
-import type { ResponsiveData } from "../features/profile/hook/useInfoUser";
-
-
-export const getInfoUser = async (): Promise<ResponsiveData> => {
-    return await get({
-        url: "auth/getProfile"
-    })
-}
